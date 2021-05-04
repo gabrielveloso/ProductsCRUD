3d4ea9da5754bbb9a4846c3973f9283ffa4dd94c
Gabriel Veloso <gabriel.azuirson@ufpe.br>
2021-05-04 20:18:17 -0300

    configurating environment and environment.prod

--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -4,13 +4,14 @@ import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Observable } from 'rxjs';
 import { Product } from './product.model';
+import { environment} from './../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  baseUrl = "http://localhost:3001/products"
+  baseUrl = environment.apiUrl;
 
   constructor(private snackbar: MatSnackBar, private http: HttpClient) {
 
